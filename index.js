2ee250f2b1b8cf2a8aaa34ec6ef6fd44d24edabc
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-05 14:37:52 +0300

    Completed assignment:

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return 264 * Math.abs(start - destination);
+}
+function calculatesFarePrice(start, destination) {
+    let feetDistance = distanceTravelledInFeet(start, destination);
+    if (feetDistance < 401) {
+        return 0;
+    } else if (feetDistance > 400 && feetDistance < 2001) {
+        return (feetDistance - 400) * 0.02;
+    } else if (feetDistance > 2000 && feetDistance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
